d4f39bfd3ce6379f731b58a4947314d1527c00b1
Narayan <7tigerpowers@gmail.com>
2026-09-13 00:42:04 +0530

    DM rgb working

--- keyboards/wuque/ikki68_aurora/keymaps/narayan/keymap.c
+++ keyboards/wuque/ikki68_aurora/keymaps/narayan/keymap.c
@@ -1,19 +1,3 @@
-/* Copyright 2021 wuquestudio
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 #include "timer.h"
 #include "rgblight.h"
@@ -23,13 +7,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_all(
         QK_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,    KC_DEL, KC_PSCR,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,             KC_BRID, KC_BRIU,
-        QK_LOCK, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-        KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, RSFT_T(KC_CAPS),     KC_UP,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  RSFT_T(KC_CAPS), RSFT_T(KC_CAPS),     KC_UP,
         KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,           KC_MPLY,           KC_SPC,           MO(1), MO(2),   MO(3),             KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [1] = LAYOUT_all(
         _______, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,    _______,_______,
-        _______, MS_BTN1, MS_UP, MS_BTN2, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______, _______,
+        _______, MS_BTN1, MS_UP, MS_BTN2, _______, _______, _______, RGB_M_SW, RGB_M_SN, _______, _______, _______, _______, VK_TOGG,             _______, _______,
         AC_TOGG, MS_LEFT, MS_DOWN, MS_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             KC_VOLU,
         _______, _______, _______,          _______,          _______,          _______,          XXXXXXX, XXXXXXX, XXXXXXX,             KC_MPRV, KC_VOLD, KC_MNXT
@@ -50,6 +34,51 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 // ==================================================
+// RGB Light status layers
+// ==================================================
+
+enum rgb_status_layers {
+    RGB_LAYER_DYNAMIC_RECORD,
+    RGB_LAYER_DYNAMIC_FLASH,
+    RGB_LAYER_DYNAMIC_STOP,
+    RGB_LAYER_AUTOCORRECT_ON,
+    narayan,
+};
+
+const rgblight_segment_t PROGMEM rgb_dynamic_record_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, RGBLIGHT_LED_COUNT, HSV_GREEN}
+);
+
+const rgblight_segment_t PROGMEM rgb_dynamic_flash_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, RGBLIGHT_LED_COUNT, HSV_WHITE}
+);
+
+const rgblight_segment_t PROGMEM rgb_dynamic_stop_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, RGBLIGHT_LED_COUNT, HSV_RED}
+);
+
+const rgblight_segment_t PROGMEM rgb_autocorrect_on_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, RGBLIGHT_LED_COUNT, HSV_GREEN}
+);
+
+const rgblight_segment_t PROGMEM rgb_autocorrect_off_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, RGBLIGHT_LED_COUNT, HSV_RED}
+);
+
+// Later entries take precedence when layers overlap.
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    rgb_dynamic_record_layer,
+    rgb_dynamic_flash_layer,
+    rgb_dynamic_stop_layer,
+    rgb_autocorrect_on_layer,
+    rgb_autocorrect_off_layer
+);
+
+void keyboard_post_init_user(void) {
+    rgblight_layers = my_rgb_layers;
+}
+
+// ==================================================
 // Dynamic Macro LED feedback
 // ==================================================
 
@@ -60,66 +89,24 @@ static uint16_t dynamic_macro_flash_timer = 0;
 static bool dynamic_macro_end_animation = false;
 static uint16_t dynamic_macro_end_timer = 0;
 
-// Saved RGB state
-static bool dynamic_macro_saved_enabled;
-static uint8_t dynamic_macro_saved_mode;
-static uint8_t dynamic_macro_saved_hue;
-static uint8_t dynamic_macro_saved_sat;
-static uint8_t dynamic_macro_saved_val;
-static uint8_t dynamic_macro_saved_speed;
-
-
-static void dynamic_macro_save_rgb_state(void) {
-    dynamic_macro_saved_enabled = rgblight_is_enabled();
-    dynamic_macro_saved_mode = rgblight_get_mode();
-    dynamic_macro_saved_hue = rgblight_get_hue();
-    dynamic_macro_saved_sat = rgblight_get_sat();
-    dynamic_macro_saved_val = rgblight_get_val();
-    dynamic_macro_saved_speed = rgblight_get_speed();
-}
-
-
-static void dynamic_macro_restore_rgb_state(void) {
-    if (dynamic_macro_saved_enabled) {
-        rgblight_enable_noeeprom();
-    } else {
-        rgblight_disable_noeeprom();
-    }
-
-    rgblight_sethsv_noeeprom(
-        dynamic_macro_saved_hue,
-        dynamic_macro_saved_sat,
-        dynamic_macro_saved_val
-    );
-
-    rgblight_mode_noeeprom(dynamic_macro_saved_mode);
-    rgblight_set_speed_noeeprom(dynamic_macro_saved_speed);
-}
-
 
 bool dynamic_macro_record_start_user(int8_t direction) {
     dynamic_macro_recording = true;
     dynamic_macro_end_animation = false;
 
-    // Save the user's current RGB state
-    dynamic_macro_save_rgb_state();
-
-    // Recording started -> solid green
-    rgblight_enable_noeeprom();
-    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-    rgblight_sethsv_noeeprom(HSV_GREEN);
+    // Clear any stale indicator layers, then show green while recording.
+    rgblight_set_layer_state(RGB_LAYER_DYNAMIC_FLASH, false);
+    rgblight_set_layer_state(RGB_LAYER_DYNAMIC_STOP, false);
+    rgblight_set_layer_state(RGB_LAYER_DYNAMIC_RECORD, true);
 
     return true;
 }
 
 
 bool dynamic_macro_record_key_user(int8_t direction, keyrecord_t *record) {
-    // Flash white on key press
+    // Flash white on key press.
     if (record->event.pressed) {
-        rgblight_enable_noeeprom();
-        rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-        rgblight_sethsv_noeeprom(HSV_WHITE);
-
+        rgblight_set_layer_state(RGB_LAYER_DYNAMIC_FLASH, true);
         dynamic_macro_flash_timer = timer_read();
     }
 
@@ -130,12 +117,12 @@ bool dynamic_macro_record_key_user(int8_t direction, keyrecord_t *record) {
 bool dynamic_macro_record_end_user(int8_t direction) {
     dynamic_macro_recording = false;
 
-    // Recording stopped -> solid red
-    rgblight_enable_noeeprom();
-    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-    rgblight_sethsv_noeeprom(HSV_RED);
+    // Stop the recording layer and show solid red.
+    rgblight_set_layer_state(RGB_LAYER_DYNAMIC_RECORD, false);
+    rgblight_set_layer_state(RGB_LAYER_DYNAMIC_FLASH, false);
+    rgblight_set_layer_state(RGB_LAYER_DYNAMIC_STOP, true);
 
-    // Keep red visible for 300 ms
+    // Keep red visible for 300 ms.
     dynamic_macro_end_animation = true;
     dynamic_macro_end_timer = timer_read();
 
@@ -150,51 +137,11 @@ bool dynamic_macro_record_end_user(int8_t direction) {
 static bool autocorrect_led_animation = false;
 static uint16_t autocorrect_led_timer = 0;
 
-// Saved RGB state
-static bool autocorrect_saved_enabled;
-static uint8_t autocorrect_saved_mode;
-static uint8_t autocorrect_saved_hue;
-static uint8_t autocorrect_saved_sat;
-static uint8_t autocorrect_saved_val;
-static uint8_t autocorrect_saved_speed;
-
-
-static void autocorrect_save_rgb_state(void) {
-    autocorrect_saved_enabled = rgblight_is_enabled();
-    autocorrect_saved_mode = rgblight_get_mode();
-    autocorrect_saved_hue = rgblight_get_hue();
-    autocorrect_saved_sat = rgblight_get_sat();
-    autocorrect_saved_val = rgblight_get_val();
-    autocorrect_saved_speed = rgblight_get_speed();
-}
-
-
-static void autocorrect_restore_rgb_state(void) {
-    if (autocorrect_saved_enabled) {
-        rgblight_enable_noeeprom();
-    } else {
-        rgblight_disable_noeeprom();
-    }
-
-    rgblight_sethsv_noeeprom(
-        autocorrect_saved_hue,
-        autocorrect_saved_sat,
-        autocorrect_saved_val
-    );
-
-    rgblight_mode_noeeprom(autocorrect_saved_mode);
-    rgblight_set_speed_noeeprom(autocorrect_saved_speed);
-}
-
 
 static void autocorrect_led_on(void) {
-    // Save current RGB state
-    autocorrect_save_rgb_state();
-
     // Autocorrect ON -> green
-    rgblight_enable_noeeprom();
-    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-    rgblight_sethsv_noeeprom(HSV_GREEN);
+    rgblight_set_layer_state(RGB_LAYER_AUTOCORRECT_OFF, false);
+    rgblight_set_layer_state(RGB_LAYER_AUTOCORRECT_ON, true);
 
     autocorrect_led_animation = true;
     autocorrect_led_timer = timer_read();
@@ -202,13 +149,9 @@ static void autocorrect_led_on(void) {
 
 
 static void autocorrect_led_off(void) {
-    // Save current RGB state
-    autocorrect_save_rgb_state();
-
     // Autocorrect OFF -> red
-    rgblight_enable_noeeprom();
-    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-    rgblight_sethsv_noeeprom(HSV_RED);
+    rgblight_set_layer_state(RGB_LAYER_AUTOCORRECT_ON, false);
+    rgblight_set_layer_state(RGB_LAYER_AUTOCORRECT_OFF, true);
 
     autocorrect_led_animation = true;
     autocorrect_led_timer = timer_read();
@@ -222,7 +165,7 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     switch (keycode) {
         case AC_TOGG:
-            // QMK has already toggled Autocorrect here
+            // QMK has already toggled Autocorrect here.
             if (autocorrect_is_enabled()) {
                 autocorrect_led_on();
             } else {
@@ -246,8 +189,8 @@ void matrix_scan_user(void) {
     if (dynamic_macro_recording &&
         timer_elapsed(dynamic_macro_flash_timer) >= 100) {
 
-        // White flash finished -> back to green
-        rgblight_sethsv_noeeprom(HSV_GREEN);
+        // White flash finished -> back to green.
+        rgblight_set_layer_state(RGB_LAYER_DYNAMIC_FLASH, false);
     }
 
 
@@ -260,8 +203,8 @@ void matrix_scan_user(void) {
 
         dynamic_macro_end_animation = false;
 
-        // Red animation finished -> restore previous RGB state
-        dynamic_macro_restore_rgb_state();
+        // Red animation finished -> clear the indicator layer.
+        rgblight_set_layer_state(RGB_LAYER_DYNAMIC_STOP, false);
     }
 
 
@@ -274,7 +217,8 @@ void matrix_scan_user(void) {
 
         autocorrect_led_animation = false;
 
-        // Animation finished -> restore previous RGB state
-        autocorrect_restore_rgb_state();
+        // Animation finished -> clear the indicator layer.
+        rgblight_set_layer_state(RGB_LAYER_AUTOCORRECT_ON, false);
+        rgblight_set_layer_state(RGB_LAYER_AUTOCORRECT_OFF, false);
     }
 }
